1c84580c6e8776bc07720b05af2938716c755e82
Stefan Lachnit <stefan.lachnit@tum.de>
2021-08-11 12:19:27 +0200

    update libmoon version and change dst mac of l2-load-latency example

--- examples/l2-load-latency.lua
+++ examples/l2-load-latency.lua
@@ -6,7 +6,7 @@ local stats  = require "stats"
 local hist   = require "histogram"
 
 local PKT_SIZE	= 60
-local ETH_DST	= "11:12:13:14:15:16"
+local ETH_DST	= "12:13:14:15:16:17"
 
 local function getRstFile(...)
 	local args = { ... }
