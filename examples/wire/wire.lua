23d8bf6322cbff7e3b57dd52be8a2d76083f3bf0
Stefan Lachnit <lachnit@net.in.tum.de>
2023-04-27 18:28:53 +0200

    fix problem with high txDesc count leading to incorrecly transmitted packets

--- examples/wire/wire.lua
+++ examples/wire/wire.lua
@@ -28,11 +28,11 @@ function master(args)
 		dpdkc.rte_vect_set_max_simd_bitwidth(512)
 	end
 
-	local dev1 = device.config({port = args.dev[1], rxQueues = 1, txQueues = 1, numBufs = 7000000, txDescs = 4096, disableOffloads = args.fast})
+	local dev1 = device.config({port = args.dev[1], rxQueues = 1, txQueues = 1, numBufs = 7000000, disableOffloads = args.fast})
 
 	local dev2 = nil
 	if args.dev[1] ~= args.dev[2] then
-		dev2 = device.config({port = args.dev[2], rxQueues = 1, txQueues = 1, txDescs = 4096, disableOffloads = args.fast})
+		dev2 = device.config({port = args.dev[2], rxQueues = 1, txQueues = 1, disableOffloads = args.fast})
 	else
 		dev2 = dev1
 	end
