fc54cc671492baa6ceff731daf45343a15b94123
Darshil Kaneria <dkaneria@amazon.com>
2026-01-09 23:24:01 +0000

    Update build for arm compatibility
    Signed-off-by: Darshil Kaneria <dkaneria@amazon.com>

--- clean.sh
+++ clean.sh
@@ -7,9 +7,9 @@ cd libmoon
 rm -rf build CMakeCache.txt CMakeFiles Makefile cmake_install.cmake
 rm -rf deps/dpdk/arm64-native-linux-gcc
 rm -rf deps/dpdk/x86_64-native-linux-gcc
-cd deps/luajit && make clean 2>/dev/null
-cd ../dpdk-kmods/linux/igb_uio && make clean 2>/dev/null
-cd ../../../lua/lib/turbo && make clean 2>/dev/null
+(cd deps/luajit && make clean 2>/dev/null)
+(cd deps/dpdk-kmods/linux/igb_uio && make clean 2>/dev/null)
+(cd lua/lib/turbo && make clean 2>/dev/null)
 )
 
 rm -rf build CMakeCache.txt CMakeFiles Makefile cmake_install.cmake
