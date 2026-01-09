cfa911100fc1de14ddb7dcaecf548e6f3293589d
Darshil Kaneria <dkaneria@amazon.com>
2026-01-09 23:08:01 +0000

    Add a simple clean script
    Signed-off-by: Darshil Kaneria <dkaneria@amazon.com>

---
+++ clean.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd $(dirname "${BASH_SOURCE[0]}")
+
+(
+cd libmoon
+rm -rf build CMakeCache.txt CMakeFiles Makefile cmake_install.cmake
+rm -rf deps/dpdk/arm64-native-linux-gcc
+rm -rf deps/dpdk/x86_64-native-linux-gcc
+cd deps/luajit && make clean 2>/dev/null
+cd ../dpdk-kmods/linux/igb_uio && make clean 2>/dev/null
+cd ../../../lua/lib/turbo && make clean 2>/dev/null
+)
+
+rm -rf build CMakeCache.txt CMakeFiles Makefile cmake_install.cmake
+mkdir -p build
